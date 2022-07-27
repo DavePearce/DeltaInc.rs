5e4bcd0a272e30dedc6abc89a387c117f6ee29e7
DavePearce <dave01001110@gmail.com>
2022-07-28 11:26:08 +1200

    Add point example
    This adds a point example, and significantly refactors the API.
    Actually that doesn't look too bad now.  The next task is a more
    interesting API that really exploits incrementalisation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,84 @@
 /// A trait describing something which can be _transformed_ by
 /// applying a _delta_.  For example, an array `[0,1,2]` can be
 /// transformed into another `[3,1,2]` by applying a delta which
-/// assigns element `0` to `3`.
-trait Transformable {
+/// assigns element `0` to `3`.  **This trait describes the functional
+/// case, where the receiver is not modified**.
+pub trait Transformer {
     type Delta;
 
     /// Apply a given delta to this transformable item, yielded a
     /// potentially updated version of this item.
-    fn transform(&self,d: Self::Delta) -> Self;
+    fn transform_into(&self,d: &Self::Delta) -> Self;
 }
 
-/// A trait capturing the essence of an incremental computation.
-trait Incremental<T:Transformable> : Transformable {
-   fn update(&self, to: T, delta: Self::Delta) -> T::Delta;
+/// A trait describing something which can be _transformed_ in place
+/// by applying a _delta_.  For example, an array `[0,1,2]` can be
+/// transformed into another `[3,1,2]` by applying a delta which
+/// assigns element `0` to `3`.  **In this case, the receiver is
+/// modified in place**.
+pub trait Transformable {
+    type Delta;
+
+    /// Apply a given delta to this transformable item, yielded a
+    /// potentially updated version of this item.
+    fn transform(&mut self,d: &Self::Delta);
+}
+
+/// Provide a default trait implementation for every type which is
+/// transformable.  This first clones the item, and then transforms
+/// it in place.
+impl<T: Transformable + Clone> Transformer for T {
+    type Delta = T::Delta;
+
+    fn transform_into(&self,d: &Self::Delta) -> Self {
+        // Clone
+        let mut r = self.clone();
+        // Transform
+        r.transform(d);
+        // Done
+        r
+    }
+}
+
+/// A trait capturing the essence of an incremental computation from
+/// `self` to some item `T`.  This is similar to the `Into` trait, but
+/// with the ability to work with _deltas_.  To understand this,
+/// consider the following:
+///
+/// ```Rust
+/// let t1 : T = self.into()
+/// //
+/// self.transform(&d1);
+/// //
+/// let t2 : T = self.into()
+/// ```
+///
+/// This assumes some delta `d` which can be applied to `self`.  The
+/// issue with this is that the final transformation to `t2` is
+/// _computed from scratch_.  This could be expensive, though it might
+/// be necessary.  However, for some computations, however, we can
+/// reduce the amount of work done by _incrementally_ applying `into`
+/// through the delta `d`.  That would look like this:
+///
+/// ```Rust
+/// let t1 : T = self.into()
+/// //
+/// let d2 = self.update(&t1,&d1);
+/// //
+/// let t2 = t1.transform_into(&d2);
+/// ```
+///
+/// This update version produces the same `t2` as the original, but
+/// allows for the computation to exploit existing information about
+/// `t1`.  In some cases, this can make a big difference.  For
+/// example, consider parsing algorithm which turns a _source file_
+/// into an _Abstract Syntax Tree_.  In this case, a delta is some
+/// change to the source file.  Then, given the AST for a source file
+/// and a change to that source file, the resulting AST might be
+/// almost entirely (or actually) the same as the original file.  For
+/// example, if the change removed a line of whitespace then nothing
+/// changes.
+pub trait Incremental<T:Transformer> : Transformable + Into<T> {
+   fn update(&self, to: &T, delta: &Self::Delta) -> T::Delta;
 }
 
---
+++ tests/point.rs
@@ -0,0 +1,96 @@
+use delta_inc::{Incremental,Transformable,Transformer};
+
+#[derive(PartialEq,Debug,Clone,Copy)]
+struct Point { x: i64, y: i64 }
+
+impl Point {
+    pub fn new(x: i64, y: i64) -> Self {
+        Point{x,y}
+    }
+}
+
+/// A point can be transformed by applying a "delta".  That is some
+/// amount of `x` and `y` that should be added to the point's `x` and
+/// `y`.  Thus, for a point `p={x:1,y:2}` and a delta `d={x:1,y:1}`,
+/// we would get a point `{x:2,y:3}` after applying `d` to `p`.
+impl Transformable for Point {
+    // The delta for a point is actually a point itself!    
+    type Delta = Point;
+
+    fn transform(&mut self,d: &Self::Delta) {
+        self.x += d.x;
+        self.y += d.y;
+    }    
+}
+
+/// A simple reduction from a `Point` to a `Sum` which is done just by
+/// adding the `x` and `y` fields.
+impl Into<Sum> for Point {
+    fn into(self) -> Sum {
+        Sum(self.x + self.y)
+    }
+}
+
+/// A simple example illustrating an incremental computation which
+/// reduces a point to its sum `(x+y)`.
+impl Incremental<Sum> for Point {
+    fn update(&self, to: &Sum, delta: &Self::Delta) -> <Sum as Transformer>::Delta {
+        // In this case, we can calculate the Sum delta purely from
+        // the Sum delta.
+        delta.x + delta.y
+    }
+}
+
+/// Defines a wrapper for `i64` since we cannot implement the
+/// `Transformer` trait on a type we don't own (i.e. since we don't
+/// own `Transformer` here either).
+#[derive(Clone,Copy,Debug,PartialEq)]
+struct Sum(i64);
+
+impl Transformer for Sum {
+    type Delta = i64;
+
+    fn transform_into(&self,d: &Self::Delta) -> Self {
+        Sum(self.0 + d)
+    }    
+}
+
+// ===============================================================
+// Tests
+// ===============================================================
+
+#[test]
+fn test_transformer_01() {
+    let p = Point::new(1,2);
+    let q = p.transform_into(&Point::new(1,1));
+    assert_eq!(q,Point{x:2,y:3});
+}
+
+#[test]
+fn test_transformable_01() {
+    let mut p = Point::new(1,2);
+    p.transform(&Point::new(1,1));
+    assert_eq!(p,Point{x:2,y:3});
+}
+
+#[test]
+fn test_incremental_01() {
+    let p = Point::new(1,2);
+    let d = p.update(&Sum(3),&Point::new(1,1));
+    assert_eq!(d,2);
+}
+
+#[test]
+fn test_incremental_02() {
+    let p1 = Point::new(2,3);
+    let s1 : Sum = p1.into();
+    //
+    let d1 = Point::new(1,2);
+    //
+    let p2 = p1.transform_into(&d1);
+    let s2 : Sum = p2.into();
+    //
+    let d2 = p1.update(&s1,&d1);
+    let s3 = s1.transform_into(&d2);
+    assert_eq!(s2,s3);
+}
