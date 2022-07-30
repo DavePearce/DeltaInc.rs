c23d1028a7c28f4055ea2cfa4c9fd3a8cd9fdbe6
DavePearce <dave01001110@gmail.com>
2022-07-30 15:55:39 +1200

    Refactor VecDelta into vec::Delta. #6
    This also changes the various methods for constructing a delta to make it a
    little more ergonomically pleasant.  There is still more to do on the API
    though.

--- src/vec.rs
+++ src/vec.rs
@@ -3,36 +3,69 @@ use std::ops::Range;
 use crate::{Transformable};
 use crate::region::Region;
 
-struct VecRewrite<T> {
+// ===================================================================
+// Rewrite
+// ===================================================================
+
+/// An atomic action applied to a `Vec<T>`, such as replace one region
+/// by another or inserting one or more items, etc.
+struct Rewrite<T> {
     /// Portion of `Vec<T>` being replaced.
     region: Region,
     /// Data being used for replacement
     data: Vec<T>
 }
 
-impl<T> VecRewrite<T> {
+impl<T> Rewrite<T> {
     pub fn new(region: Region, data: Vec<T>) -> Self {
-	VecRewrite{region,data: data}
+	Rewrite{region,data: data}
     }
 }
 
-pub struct VecDelta<T> {
+// ===================================================================
+// Delta
+// ===================================================================
+
+/// A set of zero or more rewrites which can be applied atomically to
+/// transform a vector.
+pub struct Delta<T> {
     /// List of patches in sorted order.
-    rewrites: Vec<VecRewrite<T>>
+    rewrites: Vec<Rewrite<T>>
 }
 
-impl<T> VecDelta<T> {
-    pub fn replace(range: Range<usize>, data: Vec<T>) -> Self {
-	let rw = VecRewrite::new(range.into(),data);
-	VecDelta{rewrites: vec![rw]}
-    }    
+impl<T> Delta<T> {
     pub fn and_replace(&mut self, range: Range<usize>, data: Vec<T>) {
-    	self.rewrites.push(VecRewrite::new(range.into(),data));
+    	self.rewrites.push(Rewrite::new(range.into(),data));
     }
 }
+
+/// Constract a delta which inserts a given range of elements at a
+/// given point in the vector.
+pub fn insert<T>(index: usize, data: Vec<T>) -> Delta<T> {
+    let rw = Rewrite::new(Region::new(index,0),data);
+    Delta{rewrites: vec![rw]}
+}    
+
+/// Construct a delta which replaces a given range of elements with
+/// another sequence of zero or more items.
+pub fn replace<T>(range: Range<usize>, data: Vec<T>) -> Delta<T> {
+    let rw = Rewrite::new(range.into(),data);
+    Delta{rewrites: vec![rw]}
+}    
+
+/// Construct a delta which removes one or more elements from the
+/// vector.
+pub fn remove<T>(range: Range<usize>) -> Delta<T> {
+    let rw = Rewrite::new(range.into(),Vec::new());
+    Delta{rewrites: vec![rw]}
+}
+
+// ===================================================================
+// Transformable
+// ===================================================================
 
 impl<T:Default + std::clone::Clone> Transformable for Vec<T> {
-    type Delta = VecDelta<T>;
+    type Delta = Delta<T>;
     
     fn transform(&mut self,d: &Self::Delta) {
 	// NOTE: this is a very inefficient implementation which I
--- tests/vec.rs
+++ tests/vec.rs
@@ -1,15 +1,16 @@
-use delta_inc::{Transformable,VecDelta};
+use delta_inc::Transformable;
+use delta_inc::vec;
 
 // ===============================================================
-// Tests
+// Replace
 // ===============================================================
 
 #[test]
-fn test_vec_01() {
+fn test_replace_01() {
     // Check unit replacement.
     let mut v1 = vec![0,2,3];
     // Construct delta
-    let mut d = VecDelta::replace(0..1,vec![1]);
+    let mut d = vec::replace(0..1,vec![1]);
     // Apply delta
     v1.transform(&d);
     // Check outcome!
@@ -17,23 +18,11 @@ fn test_vec_01() {
 }
 
 #[test]
-fn test_vec_02() {
-    // Check insertion.
-    let mut v1 = vec![1,2,3];
-    // Construct delta
-    let mut d = VecDelta::replace(0..0,vec![0]);
-    // Apply delta
-    v1.transform(&d);
-    // Check outcome!
-    assert_eq!(vec![0,1,2,3],v1);
-}
-
-#[test]
-fn test_vec_03() {
+fn test_replace_02() {
     // Check multi-replacement
     let mut v1 = vec![1,2,3];
     // Construct delta
-    let mut d = VecDelta::replace(0..2,vec![1,0]);
+    let mut d = vec::replace(0..2,vec![1,0]);
     // Apply delta
     v1.transform(&d);
     // Check outcome!
@@ -41,11 +30,11 @@ fn test_vec_03() {
 }
 
 #[test]
-fn test_vec_04() {
-    // Check deletion
+fn test_replace_03() {
+    // Check negative sized replacement
     let mut v1 = vec![1,2,3];
     // Construct delta
-    let mut d = VecDelta::replace(0..2,vec![0]);
+    let mut d = vec::replace(0..2,vec![0]);
     // Apply delta
     v1.transform(&d);
     // Check outcome!
@@ -53,11 +42,11 @@ fn test_vec_04() {
 }
 
 #[test]
-fn test_vec_05() {
+fn test_replace_04() {
     // Check multi replacement
     let mut v1 = vec![1,2,3];
     // Construct delta
-    let mut d = VecDelta::replace(0..0,vec![0]);
+    let mut d = vec::replace(0..0,vec![0]);
     // Append replacement
     d.and_replace(1..3,vec![4,5,6]);
     // Apply delta
@@ -66,3 +55,34 @@ fn test_vec_05() {
     assert_eq!(vec![0,4,5,6,3],v1);
 }
 
+// ======================================================
+// Insert
+// ======================================================
+
+#[test]
+fn test_insert_01() {
+    // Check insertion.
+    let mut v1 = vec![1,2,3];
+    // Construct delta
+    let mut d = vec::insert(0,vec![0]);
+    // Apply delta
+    v1.transform(&d);
+    // Check outcome!
+    assert_eq!(vec![0,1,2,3],v1);
+}
+
+// ======================================================
+// Remove
+// ======================================================
+
+#[test]
+fn test_remove_01() {
+    // Check negative sized replacement
+    let mut v1 = vec![1,2,3];
+    // Construct delta
+    let mut d = vec::remove(0..2);
+    // Apply delta
+    v1.transform(&d);
+    // Check outcome!
+    assert_eq!(vec![3],v1);
+}
