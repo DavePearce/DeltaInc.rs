0d27c917806326c820fcd2d8ae66d8fbcdeca397
DavePearce <dave01001110@gmail.com>
2023-08-09 12:35:20 +1200

    Minimise library
    This minimises the library somewhat

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 mod traits;
 mod region;
-pub mod seq;
 pub mod vec;
-pub mod lex;
-pub mod linear;
 
 pub use traits::{Transformable,PartiallyTransformable};
 pub use traits::{Transformer,PartialTransformer};
